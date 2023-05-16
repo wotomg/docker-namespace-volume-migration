e63706e57eef68764000fbb2098cc146b5ea44e7
wotomg <wotomg@gmail.com>
2023-05-16 11:45:39 +0300

    Added support for rootless dockerd

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 Docker volume migration to new namespace.
-By default dockerd runs containers as root. If you have added dockerd startup --userns-remap=default option, you can have some problems with volume permissions. Then this script can help you to fix permissions.
+By default dockerd runs containers as root. If you have added dockerd startup --userns-remap=default option, you can have some problems with volume permissions. Same problem you might have by running rootless docker. Then this script can help you to fix permissions.
 This script execute chown on every file (and directory) in volume, then increase UID and GID by values in /etc/subuid and /etc/subgid. This script works only with default user dockremap.
 
 How to use it:
 ```bash
-docker-ns-migration.sh /path/to/docker/volume
+docker-ns-migration.sh PATHTOVOLUME USERNAME
+```
+
+Example:
+```bash
+docker-ns-migration.sh /path/to/docker/volume dockremap
 ```
--- docker-ns-migration.sh
+++ docker-ns-migration.sh
@@ -1,31 +1,40 @@
 #!/usr/bin/env bash 
- 
-if [[ -n $1 && -e $1 ]]; then 
-  DOCKER_VOLUME=$1 
-else 
+
+if [[ -n $1 && -e $1 ]]; then
+  DOCKER_VOLUME=$1
+else
   echo "Please, specify the volume" 
-  exit 1 
-fi 
- 
-if [ "$EUID" -ne 0 ]; then 
-  read -p "You are not root! Do you want to continue? y/N?" -n 1 -r 
+  exit 1
+fi
+
+if [[ -z $(grep ^$2 /etc/subuid) && -z $(grep ^$2 /etc/subgid) ]]; then
+  echo "There is no user $2 in /etc/subuid or /etc/subgid"
+  exit 1
+else
+  REMAP_USER=$2
+fi
+
+
+if [ "$EUID" -ne 0 ]; then
+  read -p "You are not root! Do you want to continue? y/N?" -n 1 -r
   echo
-  if [[ ! $REPLY =~ ^[Yy]$ ]]; then 
-    exit 1 || return 1 
-  fi 
-fi 
- 
-SUB_UID=$(grep ^dockremap\: /etc/subuid | sed -e 's/:/ /g' | awk '{print $2}') 
-SUB_GID=$(grep ^dockremap\: /etc/subgid | sed -e 's/:/ /g' | awk '{print $2}') 
- 
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1 || return 1
+  fi
+fi
+
+SUB_UID=$(grep ^$REMAP_USER\: /etc/subuid | sed -e 's/:/ /g' | awk '{print $2}')
+SUB_GID=$(grep ^$REMAP_USER\: /etc/subgid | sed -e 's/:/ /g' | awk '{print $2}')
+
 find $DOCKER_VOLUME  -print0 | while read -d $'\0' file 
-do 
-  FILE_UID=$(ls -dn "$file" | awk '{print $3}') 
-  FILE_GID=$(ls -dn "$file" | awk '{print $4}') 
- 
-  NEW_UID=$(echo "$FILE_UID+$SUB_UID" | bc ) 
-  NEW_GID=$(echo "$FILE_GID+$SUB_GID" | bc ) 
+do
+  FILE_UID=$(ls -dn "$file" | awk '{print $3}')
+  FILE_GID=$(ls -dn "$file" | awk '{print $4}')
+
+  NEW_UID=$(echo "$FILE_UID+$SUB_UID" | bc )
+  NEW_GID=$(echo "$FILE_GID+$SUB_GID" | bc )
 
   echo "DOING: chown $NEW_UID:$NEW_GID $file" 
-  chown $NEW_UID:$NEW_GID $file 
-done 
+  chown $NEW_UID:$NEW_GID $file
+done
+
